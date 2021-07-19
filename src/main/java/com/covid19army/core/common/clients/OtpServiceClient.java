69f7176fd84b94677bec41ba84c5e89d6dc69394
ravir_000 <ravi.rk2406@gmail.com>
2021-07-19 17:32:24 +0530

    added create method, moved mobile verification response dto to core

--- src/main/java/com/covid19army/core/common/clients/OtpServiceClient.java
+++ src/main/java/com/covid19army/core/common/clients/OtpServiceClient.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.covid19army.core.dtos.MobileVerificationQueueDto;
+import com.covid19army.core.dtos.MobileVerificationResponseDto;
 import com.covid19army.core.dtos.OtpVerificationRequestDto;
 
 @FeignClient(name="OtpServiceClient", url="${app.client.otpservice.url:http://localhost:8080}")
@@ -11,4 +13,7 @@ public interface OtpServiceClient {
 
 	@PostMapping("/motp/verify")
 	public boolean validateOtp(@RequestBody OtpVerificationRequestDto otpVerificationDto);  
+	
+	@PostMapping("/motp/create")
+	public MobileVerificationResponseDto createOtp(@RequestBody MobileVerificationQueueDto mobileVerificationQueueDto);
 }
